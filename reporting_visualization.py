dfb4ef3834ac2d7b4fdd2f189d2c043071d6bf37
D3crypT0r (Syed Rizwan Hilal Shah) <66831571+D3crypT0r@users.noreply.github.com>
2024-03-02 21:11:25 +0530

    Create reporting_visualization.py

---
+++ reporting_visualization.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+class ReportingVisualization:
+    def __init__(self):
+        pass
+
+    def generate_reports_and_visualization(self, aggregated_data):
+        pass
